f79df7fb345c2af029b2cbc114be55bea9bb19d7
smilethis <michi.baier.mb@gmail.com>
2015-07-08 18:32:14 +0200

    old navbar, now closable

--- codecatch/www/js/app.js
+++ codecatch/www/js/app.js
@@ -158,5 +158,14 @@ var CodeCatchApp = angular.module('Codecatch', ['ui.bootstrap']);
 // Function just allows numeric values as input
 $(function() {
   $('#staticParent').on('keydown', '#child', function(e){-1!==$.inArray(e.keyCode,[46,8,9,27,13,110,190])||/65|67|86|88/.test(e.keyCode)&&(!0===e.ctrlKey||!0===e.metaKey)||35<=e.keyCode&&40>=e.keyCode||(e.shiftKey||48>e.keyCode||57<e.keyCode)&&(96>e.keyCode||105<e.keyCode)&&e.preventDefault()});
-})
+});
+
+//Close navbar onclick
+$(function() {
+    $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+});
   
